3339ea6419d2917a948ba8eb0ef45ef4d5c6429f
suzakijun1 <dyong1997@gmail.com>
2023-05-22 21:34:02 -0400

    testing

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <>
-      <h1>I am App Component</h1>
+      <h1>I am App Component!!!</h1>
       <button
         onClick={() => {
           electron.notificationApi.sendNotification("My custom notification!");
