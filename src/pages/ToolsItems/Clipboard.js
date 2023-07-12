c16b18bdbbea4e94d53657e50ba3bc1a8fb59c1f
suzakijun1 <dyong1997@gmail.com>
2023-07-12 01:20:55 -0400

    need to fix clipboard

--- src/pages/ToolsItems/Clipboard.js
+++ src/pages/ToolsItems/Clipboard.js
@@ -1,6 +1,91 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const Clipboard = () => {
+// const Clipboard = () => {
+//   const [clipboards, setClipboards] = useState([]);
+
+//   useEffect(() => {
+//     // Load saved clipboards from localStorage on component mount
+//     const savedClipboards = localStorage.getItem("clipboards");
+//     if (savedClipboards) {
+//       setClipboards(JSON.parse(savedClipboards));
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     // Save clipboards to localStorage whenever they change
+//     localStorage.setItem("clipboards", JSON.stringify(clipboards));
+//   }, [clipboards]);
+
+//   const handleAddClipboard = () => {
+//     setClipboards([...clipboards, ""]);
+//   };
+
+//   const handleInputChange = (event, index) => {
+//     const updatedClipboards = [...clipboards];
+//     updatedClipboards[index] = event.target.value;
+//     setClipboards(updatedClipboards);
+//   };
+
+//   const handleCopyToClipboard = (clipboard) => {
+//     navigator.clipboard
+//       .writeText(clipboard)
+//       .then(() => {
+//         console.log("Text copied to clipboard");
+//       })
+//       .catch((error) => {
+//         console.error("Failed to copy text to clipboard:", error);
+//       });
+//   };
+
+//   return (
+//     <div
+//       className="clipboard-container"
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "flex-start",
+//       }}
+//     >
+//       <h1>Clipboard</h1>
+//       <div
+//         className="clipboards-wrapper"
+//         style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+//       >
+//         {clipboards.map((clipboard, index) => (
+//           <div
+//             className="clipboard-item"
+//             style={{ display: "flex", gap: "10px", borderRadius: "5px" }}
+//             key={index}
+//           >
+//             <input
+//               type="text"
+//               value={clipboard}
+//               onChange={(event) => handleInputChange(event, index)}
+//             />
+//             <button onClick={() => handleCopyToClipboard(clipboard)}>
+//               Copy to Clipboard
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//       <button
+//         className="add-button"
+//         style={{ alignSelf: "flex-end" }}
+//         onClick={handleAddClipboard}
+//       >
+//         Add Clipboard
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Clipboard;
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { MdDelete, MdEdit } from "react-icons/md";
+import styles from "../../styles/modules/todoItem.module.scss";
+
+const Clipboard = ({ workflow, setWorkflow }) => {
   const [clipboards, setClipboards] = useState([]);
 
   useEffect(() => {
@@ -38,43 +123,42 @@ const Clipboard = () => {
   };
 
   return (
-    <div
-      className="clipboard-container"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start",
-      }}
-    >
-      <h1>Clipboard</h1>
-      <div
-        className="clipboards-wrapper"
-        style={{ display: "flex", flexDirection: "column", gap: "10px" }}
-      >
+    <div className={styles.content__wrapper}>
+      <div className={styles.texts}>
         {clipboards.map((clipboard, index) => (
-          <div
-            className="clipboard-item"
-            style={{ display: "flex", gap: "10px", borderRadius: "5px" }}
-            key={index}
-          >
-            <input
-              type="text"
-              value={clipboard}
-              onChange={(event) => handleInputChange(event, index)}
-            />
-            <button onClick={() => handleCopyToClipboard(clipboard)}>
-              Copy to Clipboard
-            </button>
+          <div className={styles.item} key={index}>
+            <span className={styles.itemText}>
+              {index + 1}. {clipboard}
+            </span>
+            <div className={styles.todoActions}>
+              <div
+                className={styles.icon}
+                onClick={() => {
+                  // Implement edit functionality
+                }}
+              >
+                <MdEdit />
+              </div>
+              <div
+                className={styles.icon}
+                onClick={() => {
+                  // Implement delete functionality
+                }}
+              >
+                <MdDelete />
+              </div>
+            </div>
           </div>
         ))}
       </div>
-      <button
-        className="add-button"
-        style={{ alignSelf: "flex-end" }}
+      <motion.button
+        className={`${styles.addButton} ${styles.icon}`}
         onClick={handleAddClipboard}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
       >
-        Add Clipboard
-      </button>
+        +
+      </motion.button>
     </div>
   );
 };
